bae2cd15e58b949a1cf32d4fa9269d27af3e8190
Xavier Agostini <xavier.agostini@mail.mcgill.ca>
2023-04-30 19:11:55 -0400

    add in crypto endpoint

---
+++ src/pages/api/crypto.ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+// GET /api/crypto
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    const coinMarketDataRequest = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=10', {
+      headers: {
+        'X-CMC_PRO_API_KEY': process.env.COINMARKETCAP_API_KEY || '',
+      }
+    })
+    const data = await coinMarketDataRequest.json()
+    return res.status(200).json(data)
+  } catch (error: any) {
+    console.error("Error going GET /crypto", error)
+    return res.status(400).send(error.message)
+  }
+}
