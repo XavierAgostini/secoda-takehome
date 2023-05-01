8cbb5a6fddb13ad9af94d62cf673687365bbb558
Xavier Agostini <xavier.agostini@mail.mcgill.ca>
2023-04-30 20:17:06 -0400

    format table with icons and format numbers

--- src/pages/CryptoTable.tsx
+++ src/pages/CryptoTable.tsx
@@ -1,6 +1,29 @@
 import { useState, useEffect } from "react"
 import { CryptoItem } from "./types"
 import { Table } from '@mantine/core';
+import Icon from './components/Icon';
+
+const { format: currencyFormatter} = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+})
+
+
+const PercentItem = ({ value }: { value: number }) => {
+  const isPositive = value > 0
+  const color = isPositive ? 'green' : 'red'
+  const textColor = isPositive ? 'text-green-500' : 'text-red-500'
+
+  const percentStr = `${value.toFixed(2)}%`.replace('-', '')
+  return (
+    <div className="flex items-center">
+      <Icon name={isPositive ? 'arrow-up' : 'arrow-down'} color={isPositive ? 'green' : 'red'} size={12}/>
+      <span className={textColor}>
+        {percentStr}
+      </span>
+    </div>
+  )
+}
 
 export default function CryptoTable() {
   const [isLoading, setIsLoading] = useState<boolean>(false)
@@ -42,13 +65,23 @@ export default function CryptoTable() {
           {cryptoListInfo.map((coin: any) => (
             <tr key={coin.id} className="">
               <td>{coin.cmc_rank}</td>
-              <td>{coin.name}</td>
-              <td>${coin.quote.USD.price}</td>
-              <td>{coin.quote.USD.percent_change_1h}%</td>
-              <td>{coin.quote.USD.percent_change_24h}%</td>
-              <td>{coin.quote.USD.percent_change_24h}%</td>
-              <td>${coin.quote.USD.market_cap}</td>
-              <td>${coin.quote.USD.volume_24h}</td>
+              <td className="flex gap-2">
+                <Icon name={coin.slug} />
+                <strong>{coin.name}</strong>
+                <span className="text-gray-500">({coin.symbol})</span>
+              </td>
+              <td>{currencyFormatter(coin.quote.USD.price)}</td>
+              <td>
+                <PercentItem value={coin.quote.USD.percent_change_1h} />
+              </td>
+              <td>
+                <PercentItem value={coin.quote.USD.percent_change_24h} />
+              </td>
+              <td>
+                <PercentItem value={coin.quote.USD.percent_change_24h} />
+              </td>
+              <td>{currencyFormatter(coin.quote.USD.market_cap)}</td>
+              <td>{currencyFormatter(coin.quote.USD.volume_24h)}</td>
               <td>{coin.circulating_supply}</td>
             </tr>
           ))}
